51b7e0886a003bd8f258ef0fb09c36228894eaef
Joerg Nick <nick@na.uni-tuebingen.de>
2021-10-20 12:08:27 +0200

    Updated tests, added formatting.

--- Unittests/utests.py
+++ Unittests/utests.py
@@ -5,37 +5,78 @@ sys.path.append('../cqToolbox')
 #from .. import nonlinear1ddamping
 from cqtoolbox import CQModel
 
+
 class LinearScatModel(CQModel):
-	def precomputing(self,s):
-		return s**2
-	def harmonicForward(self,s,b,precomp = None):
-		return precomp*b
-	def righthandside(self,t,history = None):
-		return 8*7*t**6
-		#return 3*t**2+t**9
-	def nonlinearity(self,x,t,phi):
-		return 0*x
-		#return x**3
+    def precomputing(self,s):
+        return s**2
+    def harmonicForward(self,s,b,precomp = None):
+        return precomp*b
+    def righthandside(self,t,history = None):
+        return 8*7*t**6
+    def nonlinearity(self,x,t,phi):
+        return 0*x
+    def ex_sol(self,ts):
+        return ts**8
+
+class NonlinearScatModel(CQModel):
+    def precomputing(self,s):
+        return s**1
+    def harmonicForward(self,s,b,precomp = None):
+        return precomp*b
+    def righthandside(self,t,history = None):
+        return 3*t**2+t**9
+    def nonlinearity(self,x,t,phi):
+        return x**3
+    def ex_sol(self,ts):
+        return ts**3
+
 
 class TestCQMethods(unittest.TestCase):
-	def setUp(self):
-		self.model = LinearScatModel()
-		self.T = 1
-		self.N = 8
-		self.ex_sol = np.linspace(0,self.T,self.N+1)**8
-	def test_LinearRadauIIA_2(self):
-		m = 2
-		method = "RadauIIA-"+str(m)
-		sol,counters = self.model.simulate(self.T,self.N,method = method)
-		err = max(np.abs(sol[0,::m]-self.ex_sol))
-		self.assertLess(np.abs(err),10**(-1))
-	def test_LinearRadauIIA_3(self):
-		m = 3
-		method = "RadauIIA-"+str(m)
-		sol,counters = self.model.simulate(self.T,self.N,method = method)
-		err = max(np.abs(sol[0,::m]-self.ex_sol))
-		self.assertLess(np.abs(err),10**(-3))
+#    def setUp(self):
+#        self.model = LinearScatModel()
+#        self.modelN = NonlinearScatModel() 
+    def test_LinearRadauIIA_2(self):
+        modelL       = LinearScatModel()
+        m = 2
+        N = 8
+        T = 1
+        sol,counters = modelL.simulate(T,N,method = "RadauIIA-"+str(m))
+        exSol        = modelL.ex_sol(np.linspace(0,T,N+1))
+        err          = max(np.abs(sol[0,::m]-exSol))
+        self.assertLess(np.abs(err),10**(-2))
+
+    def test_LinearRadauIIA_3(self):
+        modelL       = LinearScatModel()
+        m = 3
+        N = 9
+        T = 1
+        sol,counters = modelL.simulate(T,N,method = "RadauIIA-"+str(m))
+        exSol        = modelL.ex_sol(np.linspace(0,T,N+1))
+        err          = max(np.abs(sol[0,::m]-exSol))
+        self.assertLess(np.abs(err),10**(-3))
+
+    def testNonlinearRadauIIA_2(self):
+        modelN       = NonlinearScatModel()
+        m = 2
+        N = 11
+        T = 2
+        sol,counters = modelN.simulate(T,N,method = "RadauIIA-"+str(m))
+        exSol        = modelN.ex_sol(np.linspace(0,T,N+1))
+        err          = max(np.abs(sol[0,::m]-exSol))
+        self.assertLess(np.abs(err),10**(-3))
+
+    def testNonlinearRadauIIA_3(self):
+        modelN       = NonlinearScatModel()
+        m = 3
+        N = 7
+        T = 2
+        sol,counters = modelN.simulate(T,N,method = "RadauIIA-"+str(m))
+        exSol        = modelN.ex_sol(np.linspace(0,T,N+1))
+        err          = max(np.abs(sol[0,::m]-exSol))
+        self.assertLess(np.abs(err),10**(-7))
+
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
 
