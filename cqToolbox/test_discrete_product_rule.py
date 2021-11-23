770f4b9c0c3c1d55eb4506012f76472a2932c3aa
joergnick <nick@na.uni-tuebingen.de>
2021-11-23 13:04:04 +0100

    Further tests regarding discrete product rule.

--- cqToolbox/test_discrete_product_rule.py
+++ cqToolbox/test_discrete_product_rule.py
@@ -13,7 +13,7 @@ import numpy as np
 T = 1
 N = 10
 tau = T*1.0/N
-rk = RKMethod("RadauIIA-2",tau)
+rk = RKMethod("RadauIIA-3",tau)
 
 tp = rk.get_time_points(T)
 tp = tp[1:]
@@ -31,13 +31,9 @@ pt_vv = td_deriv.apply_RKconvol(vv,T,method = rk.method_name)
 #
 m = rk.m
 
-shifted_uu = uu*0
-for j in range(N-1):
-    shifted_uu[(j+1)*m:(j+2)*m] = uu[j*m:(j+1)*m]
-
 invA = np.linalg.inv(rk.A)
 rule_mat = np.matmul(invA.dot(np.ones((rk.m,1))),rk.b)
-#
+print((invA - np.matmul(rule_mat,invA)).dot(np.ones((rk.m,1))))
 
 print("Residual of initial CQ: ",np.linalg.norm(tau*pt_uv[0,:m]-invA.dot(uv[:m])))
 rhs = invA.dot(uv[m:2*m])-np.matmul(rule_mat,invA).dot(uv[:m])
@@ -45,8 +41,14 @@ lhs = tau*pt_uv[0,m:2*m]
 print("One step later: ",np.linalg.norm(lhs-rhs))
 rhs = invA.dot(uu[m:2*m])-np.matmul(rule_mat,invA).dot(uu[:m])
 lhs = tau*pt_uu[0,m:2*m]
-
 print("Same for u ",np.linalg.norm(lhs-rhs))
+### PRODUCT RULE:
+print("LHS: ",pt_uv[0,m:2*m])
+print("RHS: ",pt_uu[0,m:2*m]*vv[m:2*m]+rule_mat.dot(uu[:m]*pt_vv[0,m:2*m]))
+print("RHS2: ",pt_vv[0,m:2*m]*uu[m:2*m]+rule_mat.dot(vv[:m]*pt_uu[0,m:2*m]))
+res = pt_uv[0,m:2*m] - pt_uu[0,m:2*m]*vv[m:2*m]-rule_mat.dot(uu[:m]*pt_vv[0,m:2*m])
+print(res)
+
 #print(invA.dot(uv[:m]))
 ##print(uv[:m])
 ##print(vv[1:m+1])
