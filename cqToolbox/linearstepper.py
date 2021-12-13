12b03eb1f13d4c94db90abbcd5618e8255015482
Joerg Nick <nick@na.uni-tuebingen.de>
2021-12-13 11:33:05 +0100

    Found error in test of forward evaluation. Isolated error to Newton_integrator!

--- cqToolbox/linearstepper.py
+++ cqToolbox/linearstepper.py
@@ -2,29 +2,26 @@ from cqStepper import AbstractIntegrator
 import numpy as np
 from rkmethods import RKMethod
 class ImplicitEuler(AbstractIntegrator):
-    def time_step(self,W0,j,rk,history,w_star_sol_j):
+    def time_step(self,W0,j,rk,history,w_star_sol_j,tolsolver = 0):
         tau = rk.tau
         t   = tau*j 
         rhs = np.zeros((1,m))
         for stageInd in range(m):
-            rhs[:,stageInd] = 3*(t+tau*rk.c[stageInd])**2-w_star_sol_j[:,stageInd]
+            rhs[:,stageInd] = (t+tau*rk.c[stageInd])**3-w_star_sol_j[:,stageInd]
         rhs = rk.diagonalize(rhs)
         sol = 1j*np.zeros((1,m))
         for stageInd in range(m):
             sol[:,stageInd] = W0[stageInd]**(-1)*(rhs[:,stageInd])
         sol = np.real(rk.reverse_diagonalize(sol))
-        return sol ,0
-
-    def harmonicForward(self,s,b,precomp = None):
+        return sol 
+    def harmonic_forward(self,s,b,precomp = None):
         return precomp*b
     def precomputing(self,s):
-        return s    
+        return s**(-1)
 
 
-test = RKMethod("RadauIIA-1",1) 
-print(test.m)
 int_der = ImplicitEuler()
-Am = 8
+Am = 11
 m  = 2
 T  = 1
 err = np.zeros(Am)
@@ -32,11 +29,13 @@ Ns  = np.zeros(Am)
 for j in range(Am):
     N   = 4*2**j
     Ns[j] = N
+    print(Ns)
     rk = RKMethod("RadauIIA-"+str(m),T*1.0/N)
-    sol = int_der.integrate(T,rk)
-    
-    err[j] = max(np.abs(sol[0,::m]-np.linspace(0,1,N+1)**3))
+    sol,counters = int_der.integrate(T,N,method = rk.method_name)
+    err[j] = max(np.abs(sol[0,::m]-3*np.linspace(0,1,N+1)**2))
+    print(err)
 import matplotlib.pyplot as plt
+print(Ns)
 print(err)
 #plt.loglog(Ns**(-1),Ns**(-1),linestyle='dashed')
 #plt.loglog(Ns**(-1),err)
